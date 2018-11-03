1f6fe8d818eb6fecdbd4aed8934433eb56abd6fd
LewisDyer <33163858+LewisDyer@users.noreply.github.com>
2018-11-03 22:39:29 +0000

    added message panel and mouse-over entity identification

---
+++ game_messages.py
@@ -0,0 +1,28 @@
+import libtcodpy as libtcod
+
+import textwrap
+
+
+class Message:
+    def __init__(self, text, colour=libtcod.white):
+        self.text = text
+        self.colour = colour
+
+
+class MessageLog:
+    def __init__(self, x, width, height):
+        self.messages = []
+        self.x = x
+        self.width = width
+        self.height = height
+
+    def add_message(self, message):
+
+        new_msg_lines = textwrap.wrap(message.text, self.width)
+
+        for line in new_msg_lines:
+            # remove top message if needed
+            if len(self.messages) == self.height:
+                del self.messages[0]
+
+            self.messages.append(Message(line, message.colour))
