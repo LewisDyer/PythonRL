9e6fb08220d97f68e51742e5d0fdb37f8b30c5ae
LewisDyer <33163858+LewisDyer@users.noreply.github.com>
2018-11-03 20:41:58 +0000

    added non-moving monsters + game states

--- entity.py
+++ entity.py
@@ -3,13 +3,23 @@ class Entity:
     A generic object to represents players, enemies, objects, items, etc...
     """
 
-    def __init__(self, x, y, char, colour):
+    def __init__(self, x, y, char, colour, name, blocks = False):
         self.x = x
         self.y = y
         self.char = char
         self.colour = colour
+        self.name = name
+        self.blocks = blocks
 
     def move(self, dx, dy):
         # Move entity by the given amount
         self.x += dx
         self.y += dy
+
+
+def get_blocking_entities_at_location(entities, dest_x, dest_y):
+    for entity in entities:
+        if entity.blocks and entity.x == dest_x and entity.y == dest_y:
+            return entity
+
+    return None
